44d84fb08d53a169932a36b539e215bedb89da36
Vinn Nduri <ndurivo@gmail.com>
2022-06-04 23:46:32 +0300

    commit tree 85c36793e729cce110296c086aa287a844ec839e

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(naame){
+    return cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift("Milo");
+}
+
+const newCats = [...cats, "Broom"];
+
+function appendCat(name){
+    return (newCats);
+}
+
+const prepCats = ["Arnold", ...cats];
+
+function prependCat(name){
+    return (prepCats);
+}
+
+const remCats = cats.slice(0, -1);
+
+function removeLastCat(){
+    return(remCats);
+}
+
+const remFirstCat = cats.slice(1);
+
+function removeFirstCat(){
+    return (remFirstCat);
+}
